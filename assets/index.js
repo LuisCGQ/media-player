92cb300d19884a4d9b1f89f7e9e679dbc59468b7
Luis García <aig.luis@gmail.com>
2020-10-31 19:35:50 -0600

    Primer commit, añadido boton de play/pause

---
+++ assets/index.js
@@ -0,0 +1,19 @@
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+
+function MediaPlayer(config) {
+  this.media = config.el;
+}
+
+MediaPlayer.prototype.handlePlayButton = function () {
+  console.log("entró");
+  if (this.media.paused) {
+    this.media.play();
+  } else {
+    this.media.pause();
+  }
+};
+
+const player = new MediaPlayer({ el: video });
+
+button.onclick = () => player.handlePlayButton();
